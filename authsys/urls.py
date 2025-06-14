448cdc42b60c21de0bdee1b9befef21434fcccc2
slaverchief <uvaysov.2005@mail.ru>
2025-06-14 13:40:30 +0300

    names for url paths

--- authsys/urls.py
+++ authsys/urls.py
@@ -5,5 +5,5 @@ from .views import *
 
 urlpatterns = [
     path('login', LoginView.as_view(), name='token_obtain_pair'), # эндпоинт для авторизации
-    path('user', UserView.as_view())
+    path('user', UserView.as_view(), name="user_api")
 ]
--- quiz/urls.py
+++ quiz/urls.py
@@ -2,10 +2,10 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path("upload", UploadCSVView.as_view()),
-    path('quiz/', QuizApiView.as_view()),
-    path("quiz/<int:pk>", GetAnswersView.as_view()),
-    path('question/<int:pk>', QuestionApiView.as_view()),
-    path('choice/<int:pk>', ChoiceAPIView.as_view()),
+    path("upload", UploadCSVView.as_view(), name="upload"),
+    path('quiz/', QuizApiView.as_view(), name="quiz_api"),
+    path("quiz/<int:pk>", GetAnswersView.as_view(), name="get_answers"),
+    path('question/<int:pk>', QuestionApiView.as_view(), name='question_api'),
+    path('choice/<int:pk>', ChoiceAPIView.as_view(), name='choice_api'),
 
 ]
